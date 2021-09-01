266e0f02a89c664170b78af7bb09810efc056e13
Turwaith <nicolasbaur@yahoo.de>
2021-09-01 09:23:17 +0200

    Initial commit

---
+++ src/TextAdventure/Main.java
@@ -0,0 +1,10 @@
+package TextAdventure;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.println("World");
+
+        //Code will follow
+    }
+}
