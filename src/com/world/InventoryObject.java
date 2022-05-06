58b781233e26183e9c75587455403872dd02fc26
Turwaith <nicolasbaur@yahoo.de>
2022-05-06 08:39:07 +0200

    Update InventoryObject.java

--- src/com/world/InventoryObject.java
+++ src/com/world/InventoryObject.java
@@ -4,11 +4,13 @@ public abstract class InventoryObject {
 
     private final int cost;
     private final String name;
+    private final String type;
 
 
-    public InventoryObject(int cost, String name){
+    public InventoryObject(int cost, String name, String type){
         this.cost = cost;
         this.name = name;
+        this.type = type;
     }
 
     public int getCost() {
@@ -18,5 +20,8 @@ public abstract class InventoryObject {
     public String getName() {
         return name;
     }
-    
+
+    public String getType() {
+        return type;
+    }
 }
